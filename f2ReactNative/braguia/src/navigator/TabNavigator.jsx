1dd7c5834cdc0dbebcb1880abc3583872746b2c8
simaobarroso <simao.barroso@outlook.pt>
2024-06-09 09:04:19 +0100

    falta melhorar interface / use state em condicoes

--- f2ReactNative/braguia/src/navigator/TabNavigator.jsx
+++ f2ReactNative/braguia/src/navigator/TabNavigator.jsx
@@ -5,6 +5,9 @@ import Search from '../screens/Search';
 import Login from '../screens/Login';
 import Settings from '../screens/Settings';
 import Emergency from '../screens/Emergency';
+import Trail from '../screens/Trail';
+import User from '../screens/User';
+import History from '../screens/History';
 import { StyleSheet } from 'react-native';
 import CustomIcon from '../components/Customicon';
 import theme from '../theme/theme';
@@ -43,6 +46,16 @@ const TabNavigator = () => {
                 <Tab.Screen name="Emergency" component={Emergency} options={{
                     tabBarIcon: ({focused, color, size}) => (
                         <Icon size={25} color="white" name="call" />
+                        )
+                }}/>
+                <Tab.Screen name="Trail" component={Trail} options={{
+                    tabBarIcon: ({focused, color, size}) => (
+                        <Icon size={25} color="white" name="directions-walk" />
+                        )
+                }}/>
+                <Tab.Screen name="History" component={History} options={{
+                    tabBarIcon: ({focused, color, size}) => (
+                        <Icon size={25} color="white" name="history" />
                         )
                 }}/>
                 </Tab.Navigator>
